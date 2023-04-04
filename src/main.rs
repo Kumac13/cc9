27dbb2155c35aaef260608fa09a91bcb6350441c
kumac13 <foohilack@gmail.com>
2023-04-04 12:29:10 +0900

    feat: add + and - operator

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,42 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprint!("引数が足りません");
+        eprint!("invaild number of arguments");
         process::exit(1);
     }
+
+    let mut p = args[1].chars().peekable();
 
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
-    println!("  mov rax, {}", args[1]);
+
+    while let Some(c) = p.next() {
+        match c {
+            '+' => {
+                if let Some(c) = p.next() {
+                    if c.is_digit(10) {
+                        println!("  add rax, {}", c);
+                    }
+                } else {
+                    eprint!("invaild character: {}", c);
+                    process::exit(1);
+                }
+            }
+            '-' => {
+                if let Some(c) = p.next() {
+                    if c.is_digit(10) {
+                        println!("  sub rax, {}", c);
+                    }
+                } else {
+                    eprint!("invaild character: {}", c);
+                    process::exit(1);
+                }
+            }
+            _ => {
+                println!("  mov rax, {}", c);
+            }
+        }
+    }
     println!("  ret");
 }
